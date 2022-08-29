ea77050f8301a67bd0e96910f63339f8fa0d9b1b
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-29 23:50:52 +0100

    go

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+
+class BaseModel:
+    """Represent the base model.
+
+    defines all common attributes/methods for other classes.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
