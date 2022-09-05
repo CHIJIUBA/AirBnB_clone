7f3b3fdb0dbe1663b98b5adce5e4f0baf9f5db12
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-09-05 22:43:49 +0100

    go

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -148,3 +148,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(old_updated_at, new_updated_at)
         self.assertEqual(old_created_at, new_created_at)
         self.assertTrue(mock_storage.save.called)
+
+if __name__ == "__main__":
+    unittest.main()
